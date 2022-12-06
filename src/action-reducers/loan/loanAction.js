38d0ec398d050671fec2d46170d5d7a966a51da9
Raj kumar <rajkumar@inclusivegrowthchain.com>
2022-12-06 13:08:53 +0530

    dummy fpo

--- src/action-reducers/loan/loanAction.js
+++ src/action-reducers/loan/loanAction.js
@@ -1,6 +1,7 @@
 import sendApiReq from '../../utils/sendApiReq';
 import endPoints from '../../utils/endPoints';
 import loanConstants from './loanConstants';
+import dpr from '../../dummy/manager/dpr';
 
 export async function UploadFile(data, onSuccess) {
   try {
@@ -21,13 +22,21 @@ export async function UploadFile(data, onSuccess) {
 export function getFpoList(onSuccess) {
   return async dispatch => {
     try {
-      const { data: { message } } = await sendApiReq({
-        url: endPoints.fpoList,
-      })
+      // const { data: { message } } = await sendApiReq({
+      //   url: endPoints.fpoList,
+      // })
 
       dispatch({
         type: loanConstants.GET_FPO,
-        payload: message
+        payload: dpr.map(d => ({
+          _id: d.id,
+          FPOid: d.loanId,
+          FPOname: d.fpo,
+          ceoName: d.name,
+          phoneNumber: d.phone,
+          email: d.email,
+          location: d.location,
+        }))
       })
       onSuccess()
 
